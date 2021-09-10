c85cfb6dea45bec2752dda2edc63d550365f8f65
szwen <sandra.guasch@gmail.com>
2021-09-10 15:21:11 +0200

    Update read_phone_script.js

--- read_phone_script.js
+++ read_phone_script.js
@@ -1,7 +1,1 @@
-setInterval(myTimer, 3000);
-function myTimer() {
-  var phone_number = document.getElementById("root_contact_details_primary_contact_number");
-  if (typeof(phone_number)!='undefined' && phone_number!=null){
-    phone_number.oninput=alert(phone_number.value);
-  }
-}
+alert(everything[18].childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].value)
